de474051a6deb83c775cd8aacdb6d39b43cb5a1d
vishIG <vish_ig2@mail.ru>
2022-01-11 14:30:38 +0500

    some work

--- scripts/start.ts
+++ scripts/start.ts
@@ -1,4 +1,14 @@
 let startButton = document.getElementById("startButton");
+startButton!.onclick = () =>{
+    let enemyMoveSpeedValue = <HTMLInputElement>document.getElementById("enemyMoveSpeed");
+    localStorage.setItem("enemyAttackSpeed", enemyMoveSpeedValue.value);
 
-let test:string = "hello";
-console.log(test);
+    let enemyAttackSpeedValue = <HTMLInputElement>document.getElementById("enemyAttackSpeed");
+    localStorage.setItem("enemySpawnSpeed", enemyMoveSpeedValue.value);
+
+    let enemySpawnSpeedValue = <HTMLInputElement>document.getElementById("enemySpawnSpeed");
+    localStorage.setItem("enemySpawnSpeed", enemySpawnSpeedValue.value);
+
+    document.location='/index.html';
+    
+}
